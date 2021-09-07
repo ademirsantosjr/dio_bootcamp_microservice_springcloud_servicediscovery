7ff370b54c5fe69577a2ec7d9416deb37be0238b
Ademir <ademir.m.junior@gmail.com>
2021-09-07 08:25:03 -0300

    Config main class and .yml

--- src/main/java/one/dio/experts/servicediscovery/ServicediscoveryApplication.java
+++ src/main/java/one/dio/experts/servicediscovery/ServicediscoveryApplication.java
@@ -2,8 +2,10 @@ package one.dio.experts.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServicediscoveryApplication {
 
 	public static void main(String[] args) {
